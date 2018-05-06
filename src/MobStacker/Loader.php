2812e33c60d08f8c1b912713a579be350bc3ccf8
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-05-06 10:16:12 +0100

    Add files via upload

--- src/MobStacker/Loader.php
+++ src/MobStacker/Loader.php
@@ -10,6 +10,7 @@ public function onLoad() : void{
     $this->getLogger()->info("Plugin is being enabled. Please wait..");
 }
 public function onEnable() : void{
+    $this->getServer()->getPluginManager()->registerEvents(new StackEvent($this), $this);
     $this->getLogger()->info("Plugin has been enabled succesfully. Looking for errors. (If no errors found, then you should be fine.");
 }
 public function onDisable() : void{
